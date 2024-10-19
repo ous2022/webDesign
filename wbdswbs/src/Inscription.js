c1d8ac192876a32d57c4ff8bd90871e11aad3043
ZanaSiaka <zanacoul210@gmail.com>
2024-10-19 11:29:30 +0000

    Page d'inscription

--- wbdswbs/src/Inscription.js
+++ wbdswbs/src/Inscription.js
@@ -1,20 +1,24 @@
-// import { Fragment } from "react"
-
-import { Link } from "react-router-dom"
-
-const Connexion = () => {
-    return( 
-        <div className='connect'>
+const Inscription = () => {
+    return(
+        <div className="connect">
             <div className="left">
-                <h1>Connexion</h1>
+                <h1>Inscription</h1>
                 <p>
-                    Connectez-vous ici pour acceder à notre site dans son integralité
+                    Inscrivez-vous ici pour acceder à notre site dans son integralité
                 </p>
 
                 <form>
+                    <div className="nom">
+                        <label htmlFor="nom">Nom Complet</label>
+                        <input type="nom" id="nom" name="nom" />
+                    </div>
                     <div className="email">
                         <label htmlFor="email">Email</label>
                         <input type="email" id="email" name="email" />
+                    </div>
+                    <div className="numero">
+                        <label htmlFor="numero">Numero de telephone</label>
+                        <input type="numero" id="numero" name="numero" />
                     </div>
                     <div className="password">
                         <label htmlFor="password">Mot de passe</label>
@@ -25,16 +29,14 @@ const Connexion = () => {
                         <button type="submit">Se connecter</button>
                     </div>
                 </form>
-                <span>
-                    Vous n'avez pas de compte ? <Link className="toco" to="/inscription">Inscrivez vous</Link>
-                </span>
             </div>
 
             <div className="right">
-                <img src="" alt="" />
+                <img src="https://i.imgur.com/6Q7b3Z9.png" alt="" />
             </div>
+
         </div>
     )
 }
 
-export default Connexion
+export default Inscription
